2047fa4042adf68a01600be549037aa6ae0d1168
H13062 <bgh1506@gmail.com>
2023-05-28 16:06:35 -0500

    add login and routing for summary, country and live

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { SummaryModule } from './summary/summary.module';
+import { CountryModule } from './country/country.module';
+import { LiveModule } from './live/live.module';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    SummaryModule,
+    CountryModule,
+    LiveModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
