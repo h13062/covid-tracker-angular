fc9e05f0103f8842c765c58017a243f1e3b5ebc5
H13062 <bgh1506@gmail.com>
2023-05-28 11:59:54 -0500

    generate shared module and compents

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SidebarComponent, TopbarComponent],
+  imports: [CommonModule],
+  exports: [SidebarComponent, TopbarComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
